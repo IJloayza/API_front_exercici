b9f321eebca8287a606fd3bbda4a9d1738130bf5
Ignacio Loayza <loayzajoseignacio3@gmail.com>
2024-10-20 21:52:15 +0200

    Adding screenshots and csv for test

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Cridem a l'endpoint de l'API fent un fetch
-    ______________________________________________
+    fetch("http://localhost:8000/alumne/list")
         .then(response => {
             if (!response.ok) {
                 throw new Error("Error a la resposta del servidor");
@@ -18,9 +18,21 @@ document.addEventListener("DOMContentLoaded", function() {
                 const nomAluCell = document.createElement("td");
                 nomAluCell.textContent = alumne.NomAlumne;
                 row.appendChild(nomAluCell);
+                const cicleAluCell = document.createElement("td");
+                cicleAluCell.textContent = alumne.Cicle;
+                row.appendChild(cicleAluCell);
+                const cursAluCell = document.createElement("td");
+                cursAluCell.textContent = alumne.Curs;
+                row.appendChild(cursAluCell);
+                const grupAluCell = document.createElement("td");
+                grupAluCell.textContent = alumne.Grup;
+                row.appendChild(grupAluCell);
+                const descAulaCell = document.createElement("td");
+                descAulaCell.textContent = alumne.DescAula;
+                row.appendChild(descAulaCell);
 
                 // Repetir per tots els altres camps restants que retorna l'endpoint
-                _____________________________________________
+                
 
                 
 
